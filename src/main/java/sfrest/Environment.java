306568e2760c674bbfe8c84b861c6ad8b563d83d
Jeff Fang <sanlyfang@gmail.com>
2014-02-24 23:07:38 +1100

    Javadoc for Environment.java

--- src/main/java/sfrest/Environment.java
+++ src/main/java/sfrest/Environment.java
@@ -2,6 +2,12 @@ package sfrest;
 
 import java.net.URI;
 
+/**
+ * Salesforce environment, built-in environments are {@code Production} and {@code Sandbox}.
+ * <p>
+ * If login via custom domain is mandatory, can add additional environments using static {@code init} method.
+ * </p>
+ */
 public class Environment {
 
     public static final Environment PRODUCTION = new Environment("Production", "login.salesforce.com");
@@ -12,7 +18,7 @@ public class Environment {
     /**
      * Override default environments to support custom domain etc.
      */
-    public static void registerEnvironments(Environment... envs) {
+    public static void init(Environment... envs) {
         ALL_ENVS = envs;
     }
 
@@ -36,14 +42,23 @@ public class Environment {
         return host;
     }
 
+    /**
+     * For authorization, e.g. https://login.salesforce.com/services/oauth2/authorize
+     */
     public URI getAuthURI() {
         return getBaseURI().resolve("authorize");
     }
 
+    /**
+     * For token requests, e.g. https://login.salesforce.com/services/oauth2/token
+     */
     public URI getTokenURI() {
         return getBaseURI().resolve("token");
     }
 
+    /**
+     * For revoking OAuth tokens, e.g. https://login.salesforce.com/services/oauth2/revoke
+     */
     public URI getRevokeTokenURI() {
         return getBaseURI().resolve("revoke");
     }
@@ -59,5 +74,9 @@ public class Environment {
             }
         }
         return null;
+    }
+
+    public String toString() {
+        return name;
     }
 }
